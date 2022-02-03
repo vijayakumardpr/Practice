15999a32f6f42f739d2bba25e0cda888f1be1da4
vijayakumardpr <vijayakumardpr@gmail.com>
2022-02-03 21:21:09 +0530

    commit

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-function lastWorldLength(s) {
-  return s.split("")
+let mul = (a) => {    
+  return (b) => {
+    return (c) => {
+      return a * b * c
+    }
+  }
 }
 
-console.log("Hello world")
+console.log(mul(2)(3)(4))
+
+
+// Normal function
+
+// anonymous function
+

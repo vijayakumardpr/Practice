c35dc1f703523ce27a0f704a818f00601e8c947a
vijayakumardpr <vijayakumardpr@gmail.com>
2022-03-22 23:46:24 +0530

    commit

--- script.js
+++ script.js
@@ -1,77 +1,77 @@
-let pwd1 = document.querySelector(".pwd1")
-let pwd2 = document.querySelector(".pwd2")
-let pwd3 = document.querySelector(".pwd3")
-let pwd4 = document.querySelector(".pwd4")
-let btn = document.querySelector("button")
+// let pwd1 = document.querySelector(".pwd1")
+// let pwd2 = document.querySelector(".pwd2")
+// let pwd3 = document.querySelector(".pwd3")
+// let pwd4 = document.querySelector(".pwd4")
+// let btn = document.querySelector("button")
 
-let randomPasswordCharacter = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "!",
-  "#",
-  "$",
-  "%",
-  "&",
-  "*",
-  "+",
-  ",",
-  "-",
-  "?",
-  "@",
-  "[",
-  "]",
-  1,
-  2,
-  3,
-  4,
-  5,
-]
+// let randomPasswordCharacter = [
+//   "A",
+//   "B",
+//   "C",
+//   "D",
+//   "E",
+//   "a",
+//   "b",
+//   "c",
+//   "d",
+//   "e",
+//   "!",
+//   "#",
+//   "$",
+//   "%",
+//   "&",
+//   "*",
+//   "+",
+//   ",",
+//   "-",
+//   "?",
+//   "@",
+//   "[",
+//   "]",
+//   1,
+//   2,
+//   3,
+//   4,
+//   5,
+// ]
 
-let passwords1 = []
-let passwords2 = []
-let passwords3 = []
-let passwords4 = []
+// let passwords1 = []
+// let passwords2 = []
+// let passwords3 = []
+// let passwords4 = []
 
-function randomPassword() {
-  let randomNumber = Math.floor(Math.random() * randomPasswordCharacter.length)
-  return randomPasswordCharacter[randomNumber]
-}
-btn.addEventListener("click", (e) => {
-  e.preventDefault()
+// function randomPassword() {
+//   let randomNumber = Math.floor(Math.random() * randomPasswordCharacter.length)
+//   return randomPasswordCharacter[randomNumber]
+// }
+// btn.addEventListener("click", (e) => {
+//   e.preventDefault()
 
-  for (let i = 0; i < 6; i++) {
-    passwords1.push(randomPassword())
-    pwd1.value = passwords1.join("")
-  }
+//   for (let i = 0; i < 6; i++) {
+//     passwords1.push(randomPassword())
+//     pwd1.value = passwords1.join("")
+//   }
 
-  for (let i = 0; i < 6; i++) {
-    passwords2.push(randomPassword())
-    pwd2.value = passwords2.join("")
-  }
+//   for (let i = 0; i < 6; i++) {
+//     passwords2.push(randomPassword())
+//     pwd2.value = passwords2.join("")
+//   }
 
-  for (let i = 0; i < 6; i++) {
-    passwords3.push(randomPassword())
-    pwd3.value = passwords3.join("")
-  }
+//   for (let i = 0; i < 6; i++) {
+//     passwords3.push(randomPassword())
+//     pwd3.value = passwords3.join("")
+//   }
 
-  for (let i = 0; i < 6; i++) {
-    passwords4.push(randomPassword())
-    pwd4.value = passwords4.join("")
-  }
+//   for (let i = 0; i < 6; i++) {
+//     passwords4.push(randomPassword())
+//     pwd4.value = passwords4.join("")
+//   }
 
-  passwords1 = []
-  passwords2 = []
-  passwords3 = []
-  passwords4 = []
-})
+//   passwords1 = []
+//   passwords2 = []
+//   passwords3 = []
+//   passwords4 = []
+// })
 
 // function selectText() {
 //   const input = document.querySelector("input")
@@ -79,33 +79,16 @@ btn.addEventListener("click", (e) => {
 //   // document.elecCommand("copy")
 // }
 
-let number = 9629039023
-
-let last4Digits = number.toString().slice(-4)
-
-let maskedNumber = last4Digits.padStart(number.toString().length, "*")
-
-console.log(maskedNumber)
-
-var singleNumber = function (nums) {
-  const a = nums.reduce((pre, cur) => {
-    pre[cur] = ++pre[cur] || 1
-    return pre
-  }, {})
-
-  return Object.entries(a)
+function setTimeoutPromise(duration) {
+  return new Promise((resolve, reject) => {
+    resolve("resolve is a callback function")
+    setTimeout(f1, duration)
+  })
 }
+setTimeoutPromise(1000).then((res) => {
+  console.log(res)
+})
 
-console.log(singleNumber([2, 2, 1]))
-
-var removeElement = function (nums, val) {
-  var num2 = []
-  for (var i = 0; i < nums.length; i++) {
-    if (nums[i] !== val) {
-      num2.push(nums[i])
-    }
-  }
-  return num2
+function f1() {
+  console.log("I'm okay with promise")
 }
-
-console.log(removeElement([3, 2, 2, 3], 3))

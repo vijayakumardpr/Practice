824a3b13529614297705623c15414db80165c346
vijayakumardpr <vijayakumardpr@gmail.com>
2022-03-23 22:49:36 +0530

    commit

--- script.js
+++ script.js
@@ -1,94 +1,51 @@
-// let pwd1 = document.querySelector(".pwd1")
-// let pwd2 = document.querySelector(".pwd2")
-// let pwd3 = document.querySelector(".pwd3")
-// let pwd4 = document.querySelector(".pwd4")
-// let btn = document.querySelector("button")
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 
-// let randomPasswordCharacter = [
-//   "A",
-//   "B",
-//   "C",
-//   "D",
-//   "E",
-//   "a",
-//   "b",
-//   "c",
-//   "d",
-//   "e",
-//   "!",
-//   "#",
-//   "$",
-//   "%",
-//   "&",
-//   "*",
-//   "+",
-//   ",",
-//   "-",
-//   "?",
-//   "@",
-//   "[",
-//   "]",
-//   1,
-//   2,
-//   3,
-//   4,
-//   5,
-// ]
+const leadsFromLocalStroage = JSON.parse(localStorage.getItem("myLeads"))
 
-// let passwords1 = []
-// let passwords2 = []
-// let passwords3 = []
-// let passwords4 = []
-
-// function randomPassword() {
-//   let randomNumber = Math.floor(Math.random() * randomPasswordCharacter.length)
-//   return randomPasswordCharacter[randomNumber]
-// }
-// btn.addEventListener("click", (e) => {
-//   e.preventDefault()
-
-//   for (let i = 0; i < 6; i++) {
-//     passwords1.push(randomPassword())
-//     pwd1.value = passwords1.join("")
-//   }
-
-//   for (let i = 0; i < 6; i++) {
-//     passwords2.push(randomPassword())
-//     pwd2.value = passwords2.join("")
-//   }
-
-//   for (let i = 0; i < 6; i++) {
-//     passwords3.push(randomPassword())
-//     pwd3.value = passwords3.join("")
-//   }
-
-//   for (let i = 0; i < 6; i++) {
-//     passwords4.push(randomPassword())
-//     pwd4.value = passwords4.join("")
-//   }
-
-//   passwords1 = []
-//   passwords2 = []
-//   passwords3 = []
-//   passwords4 = []
-// })
-
-// function selectText() {
-//   const input = document.querySelector("input")
-//   console.log(input.value.select())
-//   // document.elecCommand("copy")
-// }
-
-function setTimeoutPromise(duration) {
-  return new Promise((resolve, reject) => {
-    resolve("resolve is a callback function")
-    setTimeout(f1, duration)
-  })
+if (leadsFromLocalStroage) {
+  myLeads = leadsFromLocalStroage
+  render(myLeads)
 }
-setTimeoutPromise(1000).then((res) => {
-  console.log(res)
+
+tabBtn.addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+  })
 })
 
-function f1() {
-  console.log("I'm okay with promise")
+function render(leads) {
+  let listItems = ""
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `<li>
+    <a target='_blank' href='${leads[i]}'>
+    ${leads[i]}
+    </a>
+    </li>`
+  }
+  ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", () => {
+  localStorage.removeItem("myLeads")
+  myLeads = []
+  render(myLeads)
+})
+
+inputBtn.addEventListener("click", function (e) {
+  if (inputEl.value === "") return
+  myLeads.push(inputEl.value)
+  inputEl.value = ""
+  save()
+  render(myLeads)
+})
+
+function save() {
+  return localStorage.setItem("myLeads", JSON.stringify(myLeads))
 }

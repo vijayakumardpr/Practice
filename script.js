c0dd8ecbfb77360487b5c0c7945eea00abd94181
vijayakumardpr <vijayakumardpr@gmail.com>
2022-03-24 14:50:54 +0530

    commit

--- script.js
+++ script.js
@@ -1,51 +1,65 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
-const deleteBtn = document.getElementById("delete-btn")
-const tabBtn = document.getElementById("tab-btn")
+// let myLeads = []
+// const inputEl = document.getElementById("input-el")
+// const inputBtn = document.getElementById("input-btn")
+// const ulEl = document.getElementById("ul-el")
+// const deleteBtn = document.getElementById("delete-btn")
+// const tabBtn = document.getElementById("tab-btn")
 
-const leadsFromLocalStroage = JSON.parse(localStorage.getItem("myLeads"))
+// const leadsFromLocalStroage = JSON.parse(localStorage.getItem("myLeads"))
 
-if (leadsFromLocalStroage) {
-  myLeads = leadsFromLocalStroage
-  render(myLeads)
-}
+// if (leadsFromLocalStroage) {
+//   myLeads = leadsFromLocalStroage
+//   render(myLeads)
+// }
 
-tabBtn.addEventListener("click", () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    myLeads.push(tabs[0].url)
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
-  })
-})
+// tabBtn.addEventListener("click", () => {
+//   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//     myLeads.push(tabs[0].url)
+//     localStorage.setItem("myLeads", JSON.stringify(myLeads))
+//     render(myLeads)
+//   })
+// })
 
-function render(leads) {
-  let listItems = ""
-  for (let i = 0; i < leads.length; i++) {
-    listItems += `<li>
-    <a target='_blank' href='${leads[i]}'>
-    ${leads[i]}
-    </a>
-    </li>`
+// function render(leads) {
+//   let listItems = ""
+//   for (let i = 0; i < leads.length; i++) {
+//     listItems += `<li>
+//     <a target='_blank' href='${leads[i]}'>
+//     ${leads[i]}
+//     </a>
+//     </li>`
+//   }
+//   ulEl.innerHTML = listItems
+// }
+
+// deleteBtn.addEventListener("dblclick", () => {
+//   localStorage.removeItem("myLeads")
+//   myLeads = []
+//   render(myLeads)
+// })
+
+// inputBtn.addEventListener("click", function (e) {
+//   if (inputEl.value === "") return
+//   myLeads.push(inputEl.value)
+//   inputEl.value = ""
+//   save()
+//   render(myLeads)
+// })
+
+// function save() {
+//   return localStorage.setItem("myLeads", JSON.stringify(myLeads))
+// }
+
+function find_max(nums) {
+  let max_num = Number.NEGATIVE_INFINITY // smaller than all other numbers
+  for (let num of nums) {
+    console.log(num)
+    if (num > max_num) {
+      // (Fill in the missing line here)
+      max_num = num
+    }
   }
-  ulEl.innerHTML = listItems
+  return 0
 }
 
-deleteBtn.addEventListener("dblclick", () => {
-  localStorage.removeItem("myLeads")
-  myLeads = []
-  render(myLeads)
-})
-
-inputBtn.addEventListener("click", function (e) {
-  if (inputEl.value === "") return
-  myLeads.push(inputEl.value)
-  inputEl.value = ""
-  save()
-  render(myLeads)
-})
-
-function save() {
-  return localStorage.setItem("myLeads", JSON.stringify(myLeads))
-}
+console.log(find_max([7, 5, 1]))

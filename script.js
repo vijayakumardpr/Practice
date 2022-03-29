8bc1269d43001e66484271532fbeed1148d7abb6
vijayakumardpr <vijayakumardpr@gmail.com>
2022-03-29 15:39:26 +0530

    commit

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-const usersName = document.getElementById("searchInput")
+const slides = document.querySelectorAll(".carousel-item")
+let slidePosition = 0
+const totalSlides = slides.length
 
-usersName.addEventListener("keyup", (e) => {
-  let searchQuery = e.target.value.toLowerCase()
-  let allNamesDOMCollection = document.querySelectorAll(".name")
-  for (let i = 0; i < allNamesDOMCollection.length; i++) {
-    const currentName = allNamesDOMCollection[i].textContent.toLowerCase()
-    if (currentName.includes(searchQuery)) {
-      allNamesDOMCollection[i].style.display = "block"
-    } else {
-      allNamesDOMCollection[i].style.display = "none"
-    }
+let prev = document.getElementById("carousel-button-prev")
+let next = document.getElementById("carousel-button-next")
+
+prev.addEventListener("click", moveToPrevSlide)
+next.addEventListener("click", moveToNextSlide)
+
+function moveToNextSlide() {
+  hideAllSlides()
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0
+  } else {
+    slidePosition = slidePosition + 1
   }
-})
+
+  slides[slidePosition].classList.add("carousel-item-visible")
+}
+
+function moveToPrevSlide() {
+  hideAllSlides()
+
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1
+  } else {
+    slidePosition = slidePosition - 1
+  }
+  slides[slidePosition].classList.add("carousel-item-visible")
+}
+
+function hideAllSlides() {
+  for (let slide of slides) {
+    slide.classList.remove("carousel-item-visible")
+    slide.classList.add("carousel-item-hidden")
+  }
+}

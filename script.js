31c7117c81ed6ea6c6f6068450a193d82d6059a0
vijayakumardpr <vijayakumardpr@gmail.com>
2022-03-30 19:22:38 +0530

    commit

--- script.js
+++ script.js
@@ -1,81 +1,104 @@
-let pwd1 = document.querySelector(".pwd1")
-let pwd2 = document.querySelector(".pwd2")
-let pwd3 = document.querySelector(".pwd3")
-let pwd4 = document.querySelector(".pwd4")
-let input = document.getElementById("input")
-let btn = document.querySelector("button")
+// Create variables for the game state
+let player1Score = 0
+let player2Score = 0
+let player1Turn = true
+let player1Count = 0
+let player2Count = 0
 
-let randomPasswordCharacter = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "!",
-  "#",
-  "$",
-  "%",
-  "&",
-  "*",
-  "+",
-  ",",
-  "-",
-  "?",
-  "@",
-  "[",
-  "]",
-  1,
-  2,
-  3,
-  4,
-  5,
-]
+// Create variables to store references to the necessary DOM nodes
+const player1Dice = document.getElementById("player1Dice")
+const player2Dice = document.getElementById("player2Dice")
+const player1Scoreboard = document.getElementById("player1Scoreboard")
+const player2Scoreboard = document.getElementById("player2Scoreboard")
+const message = document.getElementById("message")
+const rollBtn = document.getElementById("rollBtn")
+const resetBtn = document.getElementById("resetBtn")
+const count1 = document.getElementById("count")
+const count2 = document.getElementById("count2")
+const wishes = document.querySelector("h2")
 
-let passwords1 = []
-let passwords2 = []
-let passwords3 = []
-let passwords4 = []
-
-function randomPassword() {
-  let randomNumber = Math.floor(Math.random() * randomPasswordCharacter.length)
-  return randomPasswordCharacter[randomNumber]
+function showDisplayButton() {
+  rollBtn.style.display = "none"
+  resetBtn.style.display = "inline-block"
 }
-btn.addEventListener("click", (e) => {
-  for (let i = 0; i < 6; i++) {
-    passwords1.push(randomPassword())
-    pwd1.value = passwords1.join("")
+
+/* Hook up a click event listener to the Roll Dice Button. */
+rollBtn.addEventListener("click", function () {
+  const randomNumber = Math.floor(Math.random() * 6) + 1
+
+  // 1. Find out which players turn it is
+  // 2. log out the value e.g. "Player 1 rolled 5"
+  // 3. Switch the turn back to the other player
+
+  if (player1Turn) {
+    player1Score += randomNumber
+    player1Scoreboard.textContent = player1Score
+    player1Dice.textContent = randomNumber
+    player1Dice.classList.remove("active")
+    player2Dice.classList.add("active")
+    message.textContent = "Player 2 Turn"
+    count1.textContent = `Count: ${(player1Count = player1Count + 1)}`
+    console.log("player1:" + player1Count)
+  } else {
+    player2Score += randomNumber
+    player2Scoreboard.textContent = player2Score
+    player2Dice.textContent = randomNumber
+    player2Dice.classList.remove("active")
+    player1Dice.classList.add("active")
+    message.textContent = "Player 1 Turn"
+    count2.textContent = `Count: ${(player2Count = player2Count + 1)}`
+    console.log("player2:" + player2Count)
   }
 
-  for (let i = 0; i < 6; i++) {
-    passwords2.push(randomPassword())
-    pwd2.value = passwords2.join("")
+  if (player1Score >= 20 && player1Count === player2Count) {
+    message.textContent = "Player 1 has won! 🥳"
+    // message.textContent = popup("Player1 has Won! 🎉")
+    showDisplayButton()
+  } else if (player2Score >= 20) {
+    message.textContent = "Player 2 has won! 🎉"
+    // message.textContent = popup("Player2 has Won! 🥳")
+    showDisplayButton()
   }
-
-  for (let i = 0; i < 6; i++) {
-    passwords3.push(randomPassword())
-    pwd3.value = passwords3.join("")
-  }
-
-  for (let i = 0; i < 6; i++) {
-    passwords4.push(randomPassword())
-    pwd4.value = passwords4.join("")
-  }
-
-  passwords1 = []
-  passwords2 = []
-  passwords3 = []
-  passwords4 = []
+  // if ((player1Score || player2Score) >= 20) {
+  //   if (player1Score > player2Score) {
+  //     message.textContent = "Player 1 won!"
+  //     rollBtn.style.display = "none"
+  //     resetBtn.style.display = "inline-block"
+  //   } else {
+  //     message.textContent = "Player 2 won!"
+  //     rollBtn.style.display = "none"
+  //     resetBtn.style.display = "inline-block"
+  //   }
+  // }
+  player1Turn = !player1Turn
 })
 
-function copy() {
-  pwd1.select()
-  pwd2.select()
-  pwd3.select()
-  pwd4.select()
-  document.execCommand("copy")
+resetBtn.addEventListener("click", reset)
+
+function reset() {
+  player1Score = 0
+  player2Score = 0
+  player1Count = 0
+  player2Count = 0
+  player1Scoreboard.textContent = player1Score
+  player2Scoreboard.textContent = player2Score
+  player1Dice.textContent = "-"
+  player2Dice.textContent = "-"
+  count1.textContent = player1Count
+  count2.textContent = player2Count
+  message.textContent = "Player 1 Turn"
+
+  player2Dice.classList.remove("active")
+  player1Dice.classList.add("active")
+  rollBtn.style.display = "inline-block"
+  resetBtn.style.display = "none"
+  player1Turn = true
+}
+
+function popup(players) {
+  wishes.textContent += players
+  wishes.style.display = "block"
+  setTimeout(() => {
+    wishes.style.display = "none"
+  }, 3000)
 }

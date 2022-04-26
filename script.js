8ae9dbccd9fb64bb471810b53d34ba7b9865bf15
vijayakumardpr <vijayakumardpr@gmail.com>
2022-04-26 22:53:24 +0530

    commit

--- script.js
+++ script.js
@@ -1,51 +1,71 @@
-const title = document.getElementById("title")
-const body = document.getElementById("body")
-const form = document.getElementById("new-post")
+const form = document.getElementById("blog-form")
+const title = document.getElementById("blog-title")
+const body = document.getElementById("blog-body")
+const search = document.getElementById("text")
+const blogContainer = document.getElementById("blog-container")
+let postArray = []
 
-let postsArray = []
-
-function renderPosts() {
+function renderPost() {
   let html = ""
-  for (let post of postsArray) {
+  for (let post of postArray) {
     html += `
-            <h3>${post.title}</h3>
-            <p>${post.body}</p>
-            <hr/>
-            `
+    <h3>${post.title}</h3>
+    <p>${post.body}</p>
+    <hr/>
+    `
   }
-  document.getElementById("blog-container").innerHTML = html
+
+  blogContainer.innerHTML = html
 }
+
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     postArray = data.slice(0, 5)
+//     renderPost()
+//   })
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault()
+//   if (title.value === "") return
+//   const blogTitle = title.value
+//   const blogBody = body.value
+
+//   const data = { title: blogTitle, body: blogBody }
+
+//   const options = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(data),
+//   }
+
+//   fetch("https://apis.scrimba.com/jsonplaceholder/todos", options)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       postArray.unshift(data)
+//       renderPost()
+//     })
+// })
 
 fetch("https://jsonplaceholder.typicode.com/posts")
   .then((res) => res.json())
   .then((data) => {
-    postsArray = data.slice(0, 5)
-    renderPosts()
-  })
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault()
-
-  if (title.value === "") return
-  const postTitle = title.value
-  const bodyTitle = body.value
-  const data = { title: postTitle, body: bodyTitle }
-
-  const options = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  }
-
-  fetch("https://apis.scrimba.com/jsonplaceholder/todos", options)
-    .then((res) => res.json())
-    .then((post) => {
-      postsArray.unshift(post)
-      renderPosts()
+    postArray = data.slice(0, 5)
+    renderPost()
+    search.addEventListener("input", (e) => {
+      console.log(search.value)
+      e.preventDefault()
+      let blogPostTitle = ""
+      const searchText = search.value.toLowerCase()
+      for (let post of postArray) {
+        blogPostTitle = post.title.toLowerCase()
+        if (blogPostTitle.includes(searchText)) {
+          title.style.display = "block"
+        } else {
+          title.style.display = "none"
+        }
+      }
     })
-
-  // title.value = ""
-  // body.value = ""
-
-  form.reset()
-})
+  })

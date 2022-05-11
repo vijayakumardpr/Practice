36ed08f82c1013a5d5caaa8e886161aebb239d4c
vijayakumardpr <vijayakumardpr@gmail.com>
2022-05-11 23:56:00 +0530

    commit

--- script.js
+++ script.js
@@ -1,41 +1,15 @@
-async function getPhotos() {
-  let response = await fetch("photos.json")
-  let photos = await response.json()
+// javascript
 
-  return photos
-}
+const get = element => document.getElementById(element);
 
-function getPhotosHtml(photos) {
-  let myPhotosHtml = photos
-    .map((photo) => {
-      return `<img class="my-photo"  src="https://picsum.photos/id/${photo.id}/100/100" alt="${photo.title}"/>`
-    })
-    .join("")
+let open = get("menu-btn");
+let nav = get("nav");
+let exit = get("exit-btn");
 
-  return `<div class="my-photos" >${myPhotosHtml}</div>`
-}
+open.addEventListener('click', () => {
+    nav.classList.add('open-nav');
+})
 
-getPhotos().then((photos) => {
-  document.body.innerHTML = `
-  <div class="my-gallery">
-  <img style="display: inline" class="my-photo" id="my-selected-photo"
-  src="https://picsum.photos/id/1/200/200"/>  
-  ${getPhotosHtml(photos)}
-  </div>
-  `
-
-  let myPhotoImgs = [...document.getElementsByClassName("my-photo")]
-
-  myPhotoImgs.forEach((photoImg, array) => {
-    photoImg.addEventListener("click", () => {
-      let selectedPhotoSrc = `${photoImg.src.substr(
-        0,
-        photoImg.src.length - 7
-      )}200/200`
-
-      let selectedPhoto = document.getElementById("my-selected-photo")
-      console.log(selectedPhoto)
-      selectedPhoto.src = selectedPhotoSrc
-    })
-  })
+exit.addEventListener('click', () => {
+    nav.classList.remove('open-nav');
 })
